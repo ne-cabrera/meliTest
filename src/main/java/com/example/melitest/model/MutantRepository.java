9452fe4483fba69b33d1cd7ba1b5b06f7ae3ff0f
Nicolas Cabrera <nicolas.cabrera@blackboard.com>
2021-05-24 11:40:24 -0500

    Update query

--- src/main/java/com/example/melitest/model/MutantRepository.java
+++ src/main/java/com/example/melitest/model/MutantRepository.java
@@ -8,7 +8,7 @@ public interface MutantRepository extends CrudRepository<Mutant, Integer>
     @Query(value = "SELECT IFNULL(counts.count_mutants, 0) as mutants, IFNULL(counts.count_humans, 0) as humans FROM " +
             "(SELECT " +
             "SUM(CASE WHEN is_mutant = 1 THEN 1 ELSE 0 END) AS count_mutants, " +
-            "SUM(CASE WHEN is_mutant = 0 THEN 1 ELSE 0 END ) AS count_humans from mutantes.mutant" +
+            "SUM(CASE WHEN is_mutant = 0 THEN 1 ELSE 0 END ) AS count_humans from `mutant`" +
             ") as counts", nativeQuery = true)
     StatsDTO countMutants();
 }
