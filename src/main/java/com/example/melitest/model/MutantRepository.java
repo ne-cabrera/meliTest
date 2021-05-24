150f3394cc591c525d45720e3b5eb5a555438422
Nicolas Cabrera <nicolas.cabrera@blackboard.com>
2021-05-24 11:16:01 -0500

    Fix query

--- src/main/java/com/example/melitest/model/MutantRepository.java
+++ src/main/java/com/example/melitest/model/MutantRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface MutantRepository extends CrudRepository<Mutant, Integer>
 {
-    @Query(value = "SELECT SUM(CASE WHEN is_mutant = true THEN 1 ELSE 0 END) AS mutants," +
-            "SUM(CASE WHEN is_mutant = 0 THEN 1 ELSE 0 END ) AS humans FROM mutant", nativeQuery = true)
+    @Query(value = "SELECT IFNULL(counts.count_mutants, 0) as mutants, IFNULL(counts.count_humans, 0) as humans FROM " +
+            "(SELECT " +
+            "SUM(CASE WHEN is_mutant = 1 THEN 1 ELSE 0 END) AS count_mutants, " +
+            "SUM(CASE WHEN is_mutant = 0 THEN 1 ELSE 0 END ) AS count_humans from mutantes.mutant" +
+            ") as counts", nativeQuery = true)
     StatsDTO countMutants();
 }
+
